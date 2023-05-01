7ec4b69615f3716862466057ec77a655e867ff09
danushanavod <danushanavod@outlook.com>
2023-05-01 13:05:52 +0530

    Updated train-test data dividing method finalizing NN models

--- MNN.R
+++ MNN.R
@@ -5,8 +5,6 @@ library(Metrics) # For MAE evaluation
 
 uow_consumptions <- read_excel("D:\\Coding Area\\University Projects\\Courseworks\\R-Machine-Learning-MNN---MLCW-IIT\\uow_consumption.xlsx")
 
-# ----------------- Data Pre-processing -----------------
-
 # Function to normalize data
 normalize <- function(x) {
   return((x - min(x)) / (max(x) - min(x)))
@@ -27,6 +25,8 @@ calculate_rmse <- function(actual, predicted) {
   sqrt(mean((actual - predicted)^2))
 }
 
+# ----------------- Data Pre-processing -----------------
+
 # Only keep the data from 20th hour column
 uow_consumptions_inputs_20th <- uow_consumptions[-c(1: 3)]
 
@@ -35,66 +35,49 @@ uow_consumptions_inputs_20th_norm <- as.data.frame(lapply(uow_consumptions_input
 
 # Not normalized data sets (For un-normalize step)
 original_train_data <- uow_consumptions_inputs_20th[1: 380,]
-original_test_data <- uow_consumptions_inputs_20th[381: 463,]
+original_test_data <- uow_consumptions_inputs_20th[381: 470,]
 
 original_train_data_min <- min(original_train_data)
 original_train_data_max <- max(original_train_data)
 
-# Divide testing and training data sets
-uow_consumptions_inputs_20th_norm_train <- uow_consumptions_inputs_20th_norm[1: 380, ]
-uow_consumptions_inputs_20th_norm_test <- uow_consumptions_inputs_20th_norm[381: 470, ]
+# Creating lags for the data set
+lag_1 = lag(uow_consumptions_inputs_20th_norm,1)
+lag_2 = lag(uow_consumptions_inputs_20th_norm,2)
+lag_3 = lag(uow_consumptions_inputs_20th_norm,3)
+lag_4 = lag(uow_consumptions_inputs_20th_norm,4)
+lag_7 = lag(uow_consumptions_inputs_20th_norm,7)
 
-# Creating lags for training data set
-lag_train_1 = lag(uow_consumptions_inputs_20th_norm_train,1)
-lag_train_2 = lag(uow_consumptions_inputs_20th_norm_train,2)
-lag_train_3 = lag(uow_consumptions_inputs_20th_norm_train,3)
-lag_train_4 = lag(uow_consumptions_inputs_20th_norm_train,4)
-lag_train_7 = lag(uow_consumptions_inputs_20th_norm_train,7)
+# Create time-delayed I/O metrics for the data set
+uow_consumptions_inputs_20th_norm_io_1 <- cbind(uow_consumptions_inputs_20th_norm, lag_1, lag_2, lag_3, lag_4, lag_7)
+colnames(uow_consumptions_inputs_20th_norm_io_1) <- c("original", "t1", "t2", "t3", "t4", "t7")
 
-# Creating lags for testing data set
-lag_test_1 = lag(uow_consumptions_inputs_20th_norm_test,1)
-lag_test_2 = lag(uow_consumptions_inputs_20th_norm_test,2)
-lag_test_3 = lag(uow_consumptions_inputs_20th_norm_test,3)
-lag_test_4 = lag(uow_consumptions_inputs_20th_norm_test,4)
-lag_test_7 = lag(uow_consumptions_inputs_20th_norm_test,7)
+uow_consumptions_inputs_20th_norm_io_2 <- cbind(uow_consumptions_inputs_20th_norm, lag_2, lag_3, lag_4, lag_7)
+colnames(uow_consumptions_inputs_20th_norm_io_2) <- c("original", "t2", "t3", "t4", "t7")
 
-# Create time-delayed I/O metrics for training data set
-uow_consumptions_inputs_20th_norm_io_1_train <- cbind(uow_consumptions_inputs_20th_norm_train,lag_train_1,lag_train_2,lag_train_3,lag_train_4,lag_train_7)
-colnames(uow_consumptions_inputs_20th_norm_io_1_train) <- c("original","t1","t2","t3","t4","t7")
+uow_consumptions_inputs_20th_norm_io_3 <- cbind(uow_consumptions_inputs_20th_norm, lag_1, lag_2, lag_3, lag_4)
+colnames(uow_consumptions_inputs_20th_norm_io_3) <- c("original", "t1", "t2","t3","t4")
 
-uow_consumptions_inputs_20th_norm_io_2_train <- cbind(uow_consumptions_inputs_20th_norm_train,lag_train_2,lag_train_3,lag_train_4,lag_train_7)
-colnames(uow_consumptions_inputs_20th_norm_io_2_train) <- c("original", "t2","t3","t4","t7")
-
-uow_consumptions_inputs_20th_norm_io_3_train <- cbind(uow_consumptions_inputs_20th_norm_train,lag_train_1,lag_train_2,lag_train_3,lag_train_4)
-colnames(uow_consumptions_inputs_20th_norm_io_3_train) <- c("original", "t1", "t2","t3","t4")
-
-uow_consumptions_inputs_20th_norm_io_4_train <- cbind(uow_consumptions_inputs_20th_norm_train,lag_train_1,lag_train_2,lag_train_3)
-colnames(uow_consumptions_inputs_20th_norm_io_4_train) <- c("original", "t1", "t2","t3")
-
-# Create time-delayed I/O metrics for testing data set
-uow_consumptions_inputs_20th_norm_io_1_test <- cbind(uow_consumptions_inputs_20th_norm_test,lag_test_1,lag_test_2,lag_test_3,lag_test_4,lag_test_7)
-colnames(uow_consumptions_inputs_20th_norm_io_1_test) <- c("original","t1","t2","t3","t4","t7")
-
-uow_consumptions_inputs_20th_norm_io_2_test <- cbind(uow_consumptions_inputs_20th_norm_test,lag_test_2,lag_test_3,lag_test_4,lag_test_7)
-colnames(uow_consumptions_inputs_20th_norm_io_2_test) <- c("original", "t2","t3","t4","t7")
-
-uow_consumptions_inputs_20th_norm_io_3_test <- cbind(uow_consumptions_inputs_20th_norm_test,lag_test_1,lag_test_2,lag_test_3,lag_test_4)
-colnames(uow_consumptions_inputs_20th_norm_io_3_test) <- c("original", "t1", "t2","t3","t4")
-
-uow_consumptions_inputs_20th_norm_io_4_test <- cbind(uow_consumptions_inputs_20th_norm_test,lag_test_1,lag_test_2,lag_test_3)
-colnames(uow_consumptions_inputs_20th_norm_io_4_test) <- c("original", "t1", "t2","t3")
+uow_consumptions_inputs_20th_norm_io_4 <- cbind(uow_consumptions_inputs_20th_norm, lag_1, lag_2, lag_3)
+colnames(uow_consumptions_inputs_20th_norm_io_4) <- c("original", "t1", "t2","t3")
 
 # Formatting time-delayed I/O metrics training data set removing N/As
-uow_consumptions_inputs_20th_norm_io_1_train <- uow_consumptions_inputs_20th_norm_io_1_train[complete.cases(uow_consumptions_inputs_20th_norm_io_1_train),]
-uow_consumptions_inputs_20th_norm_io_2_train <- uow_consumptions_inputs_20th_norm_io_2_train[complete.cases(uow_consumptions_inputs_20th_norm_io_2_train),]
-uow_consumptions_inputs_20th_norm_io_3_train <- uow_consumptions_inputs_20th_norm_io_3_train[complete.cases(uow_consumptions_inputs_20th_norm_io_3_train),]
-uow_consumptions_inputs_20th_norm_io_4_train <- uow_consumptions_inputs_20th_norm_io_4_train[complete.cases(uow_consumptions_inputs_20th_norm_io_4_train),]
+uow_consumptions_inputs_20th_norm_io_1 <- uow_consumptions_inputs_20th_norm_io_1[complete.cases(uow_consumptions_inputs_20th_norm_io_1),]
+uow_consumptions_inputs_20th_norm_io_2 <- uow_consumptions_inputs_20th_norm_io_2[complete.cases(uow_consumptions_inputs_20th_norm_io_2),]
+uow_consumptions_inputs_20th_norm_io_3 <- uow_consumptions_inputs_20th_norm_io_3[complete.cases(uow_consumptions_inputs_20th_norm_io_3),]
+uow_consumptions_inputs_20th_norm_io_4 <- uow_consumptions_inputs_20th_norm_io_4[complete.cases(uow_consumptions_inputs_20th_norm_io_4),]
 
-# Formatting time-delayed I/O metrics testing data set removing N/As
-uow_consumptions_inputs_20th_norm_io_1_test <- uow_consumptions_inputs_20th_norm_io_1_test[complete.cases(uow_consumptions_inputs_20th_norm_io_1_test),]
-uow_consumptions_inputs_20th_norm_io_2_test <- uow_consumptions_inputs_20th_norm_io_2_test[complete.cases(uow_consumptions_inputs_20th_norm_io_2_test),]
-uow_consumptions_inputs_20th_norm_io_3_test <- uow_consumptions_inputs_20th_norm_io_3_test[complete.cases(uow_consumptions_inputs_20th_norm_io_3_test),]
-uow_consumptions_inputs_20th_norm_io_4_test <- uow_consumptions_inputs_20th_norm_io_4_test[complete.cases(uow_consumptions_inputs_20th_norm_io_4_test),]
+# Divide testing and training data sets
+uow_consumptions_inputs_20th_norm_io_1_train <- uow_consumptions_inputs_20th_norm_io_1[1: 373, ]
+uow_consumptions_inputs_20th_norm_io_1_test <- uow_consumptions_inputs_20th_norm_io_1[-(1: 373), ]
+
+uow_consumptions_inputs_20th_norm_io_2_train <- uow_consumptions_inputs_20th_norm_io_2[1: 373, ]
+uow_consumptions_inputs_20th_norm_io_2_test <- uow_consumptions_inputs_20th_norm_io_2[-(1: 373), ]
+
+uow_consumptions_inputs_20th_norm_io_3_train <- uow_consumptions_inputs_20th_norm_io_3[1: 376, ]
+uow_consumptions_inputs_20th_norm_io_3_test <- uow_consumptions_inputs_20th_norm_io_3[-(1: 376), ]
+
+uow_consumptions_inputs_20th_norm_io_4_train <- uow_consumptions_inputs_20th_norm_io_4[1: 377, ]
+uow_consumptions_inputs_20th_norm_io_4_test <- uow_consumptions_inputs_20th_norm_io_4[-(1: 377), ]
 
 # ----------------- NN Implementation -----------------
 
